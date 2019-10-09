f164ca5b0bfdafa7e231d43e1a79d9f03685ce47
Kelvin Ng <kng00@stuy.edu>
2019-10-09 13:17:45 -0400

    updated 17_csv2db

--- 17_csv2db/db_builder.py
+++ 17_csv2db/db_builder.py
@@ -14,23 +14,24 @@ c = db.cursor()               #facilitate db ops
 
 #==========================================================
 
-c.execute("CREATE TABLE IF NOT EXISTS courses(code STRING, mark INTEGER, id INTEGER)")
-c.execute("DELETE FROM courses")
+c.execute("CREATE TABLE IF NOT EXISTS courses(code STRING, mark INTEGER, id INTEGER)") #checks to see if table exists before creating it
+c.execute("DELETE FROM courses")	#clears the table if it already has values in it
 
 with open('courses.csv', 'r') as csvCourse:
-	readCourse = csv.DictReader(csvCourse)
+	readCourse = csv.DictReader(csvCourse)	#creates a dictionary from csvCourse
 	for row in readCourse:
 		c.execute("INSERT INTO courses(code, mark, id) VALUES (' "+ row['code'] +" ' , "+ row['mark'] +", "+ row['id'] +") ")
+		#for every row in readCourse, insert the values of 'code', 'mark', and 'id' into the courses table and into their respective categories
 
 
-
-c.execute("CREATE TABLE IF NOT EXISTS students(name STRING, age INTEGER, id INTEGER)")
-c.execute("DELETE FROM students")
+c.execute("CREATE TABLE IF NOT EXISTS students(name STRING, age INTEGER, id INTEGER)") #checks to see if table exists before creating it
+c.execute("DELETE FROM students")	#clears the table if it already has values in it
 
 with open('students.csv', 'r') as csvStudent:
-	readStudent = csv.DictReader(csvStudent)
+	readStudent = csv.DictReader(csvStudent)	#creates a dictionary from csvStudent
 	for row in readStudent:
 		c.execute("INSERT INTO students(name, age, id) VALUES (' "+ row['name'] +" ' , "+ row['age'] +", "+ row['id'] +") ")
+		#for every row in readStudent, insert the values of 'name', 'age', and 'id' into the students table and into their respective categories
 
 #c.execute("SELECT * FROM students;")
 #print(c.fetchall())
