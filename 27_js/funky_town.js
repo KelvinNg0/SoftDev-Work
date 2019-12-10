b035051993e0c655c459436a30646ece8c0dd5b9
Kelvin <kng00@stuy.edu>
2019-12-10 08:43:42 -0500

    added 27_js

---
+++ 27_js/funky_town.js
@@ -0,0 +1,16 @@
+// Kelvin Ng
+// SoftDev1 pd1
+// K27 -- Sequential Progression
+// 2019-12-10
+
+function fibonacci(n) {
+  console.log('hi');
+}
+
+function gcd(a,b) {
+
+}
+
+function randomStudent() {
+
+}
