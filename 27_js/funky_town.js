e8f2aa5846a26eb12480d2afb79f69d96716c7f4
Kelvin Ng <kng00@stuy.edu>
2019-12-10 19:16:37 -0500

    done

--- 27_js/funky_town.js
+++ 27_js/funky_town.js
@@ -3,14 +3,32 @@
 // K27 -- Sequential Progression
 // 2019-12-10
 
-function fibonacci(n) {
-  console.log('hi');
+function fib(n) {
+  a = 1;
+  b = 0;
+  var temp;
+
+  while (n-1 >= 0){
+    temp = a;
+    a = a + b;
+    b = temp;
+    n--;
+  }
+
+  return b;
 }
 
 function gcd(a,b) {
+  while(b) {
+    var x = b;
+    b = a % b;
+    a = x;
+  }
 
+  return a;
 }
 
-function randomStudent() {
-
+function randStu() {
+  var students = ['bob', 'kelvin', 'hi', 'yeet', 'pepega'];
+  return students[Math.floor(Math.random() * 1000 % students.length)];
 }
