867046b1aa5a4f711091e618deb5892853abd219
Kelvin <kng00@stuy.edu>
2019-12-13 08:36:52 -0500

    added comments

--- 29_jsdom/dommy.js
+++ 29_jsdom/dommy.js
@@ -1,23 +1,23 @@
-var changeHeading = function(e) {
+var changeHeading = function(e) { // changes heading from Hello World! to what is mouseover
   var h = document.getElementById("h");
   h.innerHTML = e.target.innerHTML;
   var lis = document.getElementsByTagName("li");
 }
 
-var removeItem = function(e) {
+var removeItem = function(e) { // removes item from list and changes heading
   e.target.remove();
   document.getElementById("h").innerHTML = "Hello World!";
 }
 
 var lis = document.getElementsByTagName("li");
 
-for (var i = 0; i < lis.length; i++) {
+for (var i = 0; i < lis.length; i++) { // for every item in list, it allows removal of item and change of heading
 	lis[i].addEventListener('mouseover', changeHeading);
 	lis[i].addEventListener('mouseout', function(e) {document.getElementById("h").innerHTML = "Hello World!"});
   lis[i].addEventListener('click', removeItem);
 }
 
-var addItem = function(e) {
+var addItem = function(e) { // adds an item to the list
   var list = document.getElementById("thelist");
   var item = document.createElement("li");
   item.innerHTML = "WORD";
@@ -30,7 +30,7 @@ var addItem = function(e) {
 var button = document.getElementById("b");
 button.addEventListener("click", addItem);
 
-var fib = function(n) {
+var fib = function(n) { // calculates fib sequence
     if ( n < 2 ) {
 	return 1;
     } else {
@@ -38,19 +38,7 @@ var fib = function(n) {
     }
 }
 
-var fib = function(n){
-  if (n == 0){
-    return 0;
-  }
-  else if (n == 1){
-    return 1;
-  }
-  else{
-    return fib(n-1) + fib(n-2);
-  }
-}
-
-var addFib = function(e) {
+var addFib = function(e) { // adds next fib # to list
   console.log(e);
   var list = document.getElementById("fiblist");
   var child = list.childNodes;
